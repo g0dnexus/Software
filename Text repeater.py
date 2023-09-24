ba09760393b7e6bc615a9add3f47188832873404
g0dnexus <144482709+g0dnexus@users.noreply.github.com>
2023-09-24 22:46:28 +0300

    Text repeater.py

--- Text repeater.py
+++ Text repeater.py
@@ -1,25 +1,33 @@
-text = input("Enter the text / Введите текст: ")
-count = int(input("Enter the number of repetitions / Введите количество повторений: "))
+def run_program():
+    while True:
+        text = input("Enter the text / Введите текст: ")
+        count = int(input("Enter the number of repetitions / Введите количество повторений: "))
 
-prompt = "Choose the function / Выберите функцию:\n1. In line / В строчку\n2. In a column with numbering / В столбик с нумерацией\n3. In a column without numbering / В столбик без нумерации\n"
+        prompt = "Choose the function / Выберите функцию:\n1. In line / В строчку\n2. In a column with numbering / В столбик с нумерацией\n3. In a column without numbering / В столбик без нумерации\n"
 
-choice = input(prompt)
+        while True:
+            choice = input(prompt)
 
-if choice == "1":
-    # In line / В строчку
-    result = text * count
-    print("In line / В строчку:")
-    print(result)
-elif choice == "2":
-    # In a column with numbering / В столбик с нумерацией
-    print("In a column with numbering / В столбик с нумерацией:")
-    for i in range(1, count + 1):
-        print(f"{i}. {text}")
-elif choice == "3":
-    # In a column without numbering / В столбик без нумерации
-    print("In a column without numbering / В столбик без нумерации:")
-    for i in range(count):
-        print(text)
-else:
-    # Invalid choice / Неверный выбор
-    print("Invalid choice / Неверный выбор")
+            if choice == "1":
+                result = text * count
+                print("In line / В строчку:")
+                print(result)
+                break
+            elif choice == "2":
+                print("In a column with numbering / В столбик с нумерацией:")
+                for i in range(1, count + 1):
+                    print(f"{i}. {text}")
+                break
+            elif choice == "3":
+                print("In a column without numbering / В столбик без нумерации:")
+                for i in range(count):
+                    print(text)
+                break
+            else:
+                print("Invalid choice / Неверный выбор")
+
+        end_program = input("Do you want to end the program? (yes/no) / Хотите завершить программу? (да/нет): ")
+        if end_program.lower() == "yes" or end_program.lower() == "да":
+            break
+
+run_program()
